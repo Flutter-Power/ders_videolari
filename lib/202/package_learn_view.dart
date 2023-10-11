f435b6a990888418f99fa2e969ba2f478875a07b
yakoo0zz <yakupozgn81@gmail.com>
2023-10-12 00:38:53 +0300

    devam

--- lib/202/package_learn_view.dart
+++ lib/202/package_learn_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:youtube1/202/package/loading_bar.dart';
+import 'package:youtube1/202/package/lounch_marger.dart';
 
 class PackageLearnView extends StatefulWidget {
   const PackageLearnView({super.key});
@@ -8,13 +10,15 @@ class PackageLearnView extends StatefulWidget {
   State<PackageLearnView> createState() => _PackageLearnViewState();
 }
 
-class _PackageLearnViewState extends State<PackageLearnView> with TickerProviderStateMixin {
+class _PackageLearnViewState extends State<PackageLearnView> with TickerProviderStateMixin, LaunchMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          launchUrl(Uri.parse("https://pub.dev/"));
+        },
       ),
       body: const LoadingBar(),
     );
